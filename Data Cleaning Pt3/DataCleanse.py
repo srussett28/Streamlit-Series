43d9aa7f38bf9c5f6401a208449d037c3acb8407
srussett28 <167823549+srussett28@users.noreply.github.com>
2025-01-14 17:40:49 -0500

    shell for the data cleaning section

---
+++ Data Cleaning Pt3/DataCleanse.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Data Cleaning: A Foundation for Reliable Insights")
+image_clean = "DataCleanse.png"
+st.image(image_clean,  use_container_width=True)
+st.markdown("""
+Data cleaning is the cornerstone of any successful data project. Whether you’re analyzing trends, building models, or designing pipelines, clean data ensures accuracy, reliability, and meaningful insights.
+""")
